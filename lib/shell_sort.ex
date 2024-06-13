5c5ade21e89f3e48ed6f4115f2a436d640a8587f
eramoss <edulramos@outlook.com>
2024-06-13 08:57:40 -0300

    improve shell sort

--- lib/shell_sort.ex
+++ lib/shell_sort.ex
@@ -1,29 +1,35 @@
 defmodule GotoFlow.ShellSort do
-  def sort(list) when is_list(list) do
-    n = length(list)
-    h = h_value(n)
-    sort(list, h)
+  alias GotoFlow.InsertionSort
+  alias GotoFlow.Helpers
+  def sort(list) when length(list) <= 1, do: list
+  def sort(list), do: sort(list, div(length(list), 2))
+
+  defp sort(list, inc) do
+    groups_to_sort_in_pieces =
+      Enum.with_index(list) |> Enum.group_by(fn {_, i} -> rem(i, inc) end)
+
+    weak_sorted =
+      Enum.map(0..(inc - 1), fn i ->
+        # maps the groups to sort in pieces and sorts them with insertion sort
+        # and then merges them into the list again
+        Enum.map(groups_to_sort_in_pieces[i], fn {x, _} -> x end) |> InsertionSort.insert_iter([])
+      end)
+      |> merge
+
+    if Helpers.sorted?(weak_sorted),
+      do: weak_sorted,
+      else: sort(weak_sorted, max(trunc(inc / 2.2), 1))
   end
 
-  defp sort(list, h) do
-    if h == 1, do: insertion_sort(list), else: sort(list, h - 1)
-  end
+  # Takes a list of lists and merges them into one list
+  # The lists are "pre-sorted"
+  # The merges rule is that the first element of the first list is the smallest
+  defp merge(lists) do
+    len = length(hd(lists))
 
-  defp insertion_sort(list) do
-    insert_iter(list, [])
-  end
-
-  defp insert_iter([], sorted), do: sorted
-
-  defp insert_iter([h | t], sorted) do
-    insert_iter(t, insert(h, sorted))
-  end
-
-  defp insert(h, []), do: [h]
-  defp insert(h, [h2 | t]) when h <= h2, do: [h | [h2 | t]]
-  defp insert(h, [h2 | t]), do: [h2 | insert(h, t)]
-
-  defp h_value(n) do
-    Enum.reduce(1..n, 1, fn x, _ -> 3 * x + 1 end)
+    Enum.map(lists, fn list -> if length(list) < len, do: list ++ [nil], else: list end)
+    |> List.zip()
+    |> Enum.flat_map(fn tuple -> Tuple.to_list(tuple) end)
+    |> Enum.filter(& &1)
   end
 end
